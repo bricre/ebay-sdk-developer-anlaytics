3248ec57921789e2c5e80984ada6897d311bdd6d
Allan Sun <allan.sun@bricre.com>
2022-05-04 12:01:49 +0100

    Generated against OpenAPI on 2022-05-04T11:01:49+00:00

---
+++ src/Api/APIInterface.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Ebay\Developer\Anlaytics\Api;
+
+use OpenAPI\Runtime\APIInterface as BaseClass;
+
+interface APIInterface extends BaseClass
+{
+}
---
+++ src/Api/HttpClientInterface.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Ebay\Developer\Anlaytics\Api;
+
+use Psr\Http\Client\ClientInterface as BaseClass;
+
+interface HttpClientInterface extends BaseClass
+{
+}
